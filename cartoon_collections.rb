19392635c1400ce05517bb2d22394331a814acf2
Ruel Tingling <rvtingling@yahoo.com>
2019-09-07 22:59:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
+#def roll_call_dwarves# code an argument here
   # Your code here
+def roll_call_dwarves["doc", "dopey", "bashful", "grumpy"]
+puts roll_call_dwarves
 end
 
-def summon_captain_planet# code an argument here
+#def summon_captain_planet# code an argument here
   # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  summon_captain_planet = planeteer_calls.map do |planeteer_calls| "#{planeteer_calls.capitalize}!"
 end
 
 def long_planeteer_calls# code an argument here
